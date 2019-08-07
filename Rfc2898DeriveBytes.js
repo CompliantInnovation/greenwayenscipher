04526869c8d74e8c60e32ed494b717d9c1ba20ae
Luca Santarella <luca.santarella@gmail.com>
2019-08-07 16:24:19 -0400

    Fixed line endings

--- Rfc2898DeriveBytes.js
+++ Rfc2898DeriveBytes.js
@@ -1,4 +1,3 @@
-
 const crypto = require('crypto');
 const $key = Symbol('key');
 const $saltSize = Symbol('saltSize');
@@ -7,36 +6,40 @@ const $iterationCount = Symbol('iterationCount');
 const $position = Symbol('position');
 
 class Rfc2898DeriveBytes {
-	constructor(key, saltSize = 32, iterationCount = 1000) {
-		this[$key] = key;
-		this[$saltSize] = saltSize;
-		this[$iterationCount] = iterationCount;
-		this[$position] = 0;
-		this[$salt] = crypto.randomBytes(this[$saltSize]);
-	}
+  constructor(key, saltSize = 32, iterationCount = 1000) {
+    this[$key] = key;
+    this[$saltSize] = saltSize;
+    this[$iterationCount] = iterationCount;
+    this[$position] = 0;
+    this[$salt] = crypto.randomBytes(this[$saltSize]);
+  }
 
-	get salt() {
-		return this[$salt];
-	}
-	set salt(buffer) {
-		this[$salt] = buffer;
-	}
+  get salt() {
+    return this[$salt];
+  }
 
-	get iterationCount() {
-		return this[$iterationCount];
-	}
-	set iterationCount(count) {
-		this[$iterationCount] = count;
-	}
+  set salt(buffer) {
+    this[$salt] = buffer;
+  }
 
-	getBytes(byteCount) {
-		let position = this[$position];
-		let bytes = crypto.pbkdf2Sync(Buffer.from(this[$key]), this.salt, this.iterationCount, position + byteCount, 'sha1');
-		this[$position] += byteCount;
-		let result = Buffer.alloc(byteCount);
-		for (let i = 0; i < byteCount; i++) { result[i] = bytes[position + i]; }
-		return result;
-	}
+  get iterationCount() {
+    return this[$iterationCount];
+  }
+
+  set iterationCount(count) {
+    this[$iterationCount] = count;
+  }
+
+  getBytes(byteCount) {
+    let position = this[$position];
+    let bytes = crypto.pbkdf2Sync(Buffer.from(this[$key]), this.salt, this.iterationCount, position + byteCount, 'sha1');
+    this[$position] += byteCount;
+    let result = Buffer.alloc(byteCount);
+    for (let i = 0; i < byteCount; i++) {
+      result[i] = bytes[position + i];
+    }
+    return result;
+  }
 }
 
 module.exports = Rfc2898DeriveBytes;
